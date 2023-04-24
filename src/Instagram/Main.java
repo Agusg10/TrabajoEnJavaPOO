3342fb4ecb35346abb636ee7270ce4700c7cb725
Agustin <lopezagustin1999@hotmail.com>
2023-04-24 19:25:00 -0300

    Test Push 4 agustin

--- src/Instagram/Main.java
+++ src/Instagram/Main.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST PUSH GIT AGUSTIN");
-        System.out.printf("TEST PUSH GIT ");
+        System.out.println("TEST PULL");
     }
 }
