8f30862d37852cd2520b49a1ec3404df5dbe2b64
Agustin <lopezagustin1999@hotmail.com>
2023-04-24 20:47:03 -0300

    test ArrayList

--- src/Instagram/Main.java
+++ src/Instagram/Main.java
@@ -3,6 +3,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST PULL OK");
+        Scanner read = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Input number: ");
+            numbers.add(read.nextInt());
+        }
+        System.out.println(numbers);
+        System.out.println("Input number to search in the Array: ");
+        Integer x = read.nextInt();
+        if(numbers.contains(x)){
+            System.out.println("ArrayList contains: "+x);
+        }else{
+            System.out.println("ArrayList doesn't contains: "+x);
+        }
     }
 }
