1627c34bd54842cf1d42bb3d9b840c5dfabbf0e1
juanmurrie <murriejuan@gmail.com>
2023-04-24 16:11:10 -0300

    test

--- src/Instagram/Main.java
+++ src/Instagram/Main.java
@@ -1,28 +1,9 @@
 package Instagram;
-import java.util.*;
-
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Input number: ");
-            numbers.add(read.nextInt());
-        }
-        Collections.sort(numbers); //ordena de menor a mayor
-        System.out.println(numbers);
-        Collections.sort(numbers, Collections.reverseOrder()); //ordena de mayor a menor
-        System.out.println(numbers);
-        System.out.println("Input number to search in the Array: ");
-        Integer x = read.nextInt();
-        if(numbers.contains(x)){
-            System.out.println("ArrayList contains "+x);
-        }else{
-            System.out.println("ArrayList doesn't contains "+x);
-        }
-        TreeSet<Integer> integerTree = new TreeSet<>();
-        integerTree.addAll(numbers);
-        System.out.println(integerTree);
+
+        System.out.println("probando");
     }
 }
