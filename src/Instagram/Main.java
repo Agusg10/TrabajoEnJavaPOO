d1b707a2b85f009d171c864624f0fa4ab0970c34
juanmurrie <murrejuan@gmail.com>
2023-04-24 19:20:33 -0300

    PUSH TEST JUAN

--- src/Instagram/Main.java
+++ src/Instagram/Main.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST PUSH GIT 3");
+        System.out.println("TEST PUSH GIT JUAN");
     }
 }
