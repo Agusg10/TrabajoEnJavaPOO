1cf8fdb37208bda8f5eb4f557bd02c8efee9e28f
Agustin <lopezagustin1999@hotmail.com>
2023-04-24 19:25:23 -0300

    Test PULL agustin

--- src/Instagram/Main.java
+++ src/Instagram/Main.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TEST PULL");
+        System.out.println("TEST PULL OK");
     }
 }
