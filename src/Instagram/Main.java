b11fcf3a5a75cb6d3c8705a68f02fa5987033b4c
Agustin <lopezagustin1999@hotmail.com>
2023-04-24 21:05:14 -0300

    test TreeSet

--- src/Instagram/Main.java
+++ src/Instagram/Main.java
@@ -10,13 +10,19 @@ public class Main {
             System.out.println("Input number: ");
             numbers.add(read.nextInt());
         }
+        Collections.sort(numbers); //ordena de menor a mayor
+        System.out.println(numbers);
+        Collections.sort(numbers, Collections.reverseOrder()); //ordena de mayor a menor
         System.out.println(numbers);
         System.out.println("Input number to search in the Array: ");
         Integer x = read.nextInt();
         if(numbers.contains(x)){
-            System.out.println("ArrayList contains: "+x);
+            System.out.println("ArrayList contains "+x);
         }else{
-            System.out.println("ArrayList doesn't contains: "+x);
+            System.out.println("ArrayList doesn't contains "+x);
         }
+        TreeSet<Integer> integerTree = new TreeSet<>();
+        integerTree.addAll(numbers);
+        System.out.println(integerTree);
     }
 }
