1e36ea46ed9cb15b495e00e52b3f2690818929f1
juan_ <murriejuan@gmail.com>
2023-05-28 22:01:14 -0300

    javadoc en clase Filters

--- src/DomainClasses/Filters.java
+++ src/DomainClasses/Filters.java
@@ -1,12 +1,19 @@
 package DomainClasses;
-
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ The Filters class provides methods for filtering publications based on different criteria.
+ */
 public class Filters {
-
+    /**
+     * Filters the given list of publications based on the minimum number of likes.
+     *
+     * @param publications The list of publications to be filtered.
+     * @param minLikes     The minimum number of likes.
+     * @return An ArrayList of publications that have at least the minimum number of likes.
+     */
     public static ArrayList<Publication> filterByLikes(ArrayList<Publication> publications, int minLikes) {
         ArrayList<Publication> filteredPublications = new ArrayList<>();
         for (Publication publication : publications) {
@@ -17,6 +24,13 @@ public class Filters {
         return filteredPublications;
     }
 
+    /**
+     * Filters the given list of publications based on the maximum duration.
+     *
+     * @param publications The list of publications to be filtered.
+     * @param maxDuration  The maximum duration allowed.
+     * @return An ArrayList of publications that have a duration less than or equal to the maximum duration.
+     */
     public static ArrayList<Publication> filterByDuration(List<Publication> publications, int maxDuration) {
         ArrayList<Publication> filteredPublications = new ArrayList<>();
         for (Publication publication : publications) {
@@ -25,7 +39,7 @@ public class Filters {
                     filteredPublications.add(publication);
                 }
             } else if (publication instanceof Audio) {
-                if (((Audio)publication).getDuration() <= maxDuration) {
+                if (((Audio) publication).getDuration() <= maxDuration) {
                     filteredPublications.add(publication);
                 }
             }
@@ -33,8 +47,14 @@ public class Filters {
         return filteredPublications;
     }
 
-
-
+    /**
+     * Filters the given list of publications based on the upload date range.
+     *
+     * @param publications   The list of publications to be filtered.
+     * @param uploadDateMin  The minimum upload date.
+     * @param uploadDateMax  The maximum upload date.
+     * @return An ArrayList of publications that were uploaded within the specified date range.
+     */
     public static ArrayList<Publication> filterByUploadDate(List<Publication> publications, Date uploadDateMin, Date uploadDateMax) {
         ArrayList<Publication> filteredPublications = new ArrayList<>();
         for (Publication publication : publications) {
@@ -46,7 +66,13 @@ public class Filters {
         return filteredPublications;
     }
 
-
+    /**
+     * Filters the given list of publications based on the specified hashtag.
+     *
+     * @param publications The list of publications to be filtered.
+     * @param hashtag      The hashtag to filter by.
+     * @return An ArrayList of publications that contain the specified hashtag.
+     */
     public static ArrayList<Publication> filterByHashtag(List<Publication> publications, String hashtag) {
         ArrayList<Publication> filteredPublications = new ArrayList<>();
         for (Publication publication : publications) {
@@ -55,6 +81,4 @@ public class Filters {
             }
         }
         return filteredPublications;
-    }
-
-}
+    }}
